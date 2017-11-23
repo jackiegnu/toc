f49b6eebf53840c79ae22c3712498d33fa60c29e
Phodal Huang <h@phodal.com>
2017-11-23 08:56:42 +0800

    [PLUGIN] update toc

--- js/showdown.toc.js
+++ js/showdown.toc.js
@@ -1,166 +1,138 @@
-// Author: ravisorg
-// GitHub https://github.com/ravisorg/showdown-toc
-//
-
-(function (extension) {
+// Extension loading compatible with AMD and CommonJs
+(function(extension) {
   'use strict';
 
-  if (typeof showdown !== 'undefined') {
-    extension(showdown);
+  if (typeof showdown === 'object') {
+    // global (browser or nodejs global)
+    showdown.extension('toc', extension());
   } else if (typeof define === 'function' && define.amd) {
-    define(['showdown'], extension);
+    // AMD
+    define('toc', extension());
   } else if (typeof exports === 'object') {
-    module.exports = extension(require('showdown'));
+    // Node, CommonJS-like
+    module.exports = extension();
   } else {
+    // showdown was not found so we throw
     throw Error('Could not find showdown library');
   }
 
-}(function (showdown) {
-  'use strict';
+}(function() {
 
-  showdown.extension('showdown-toc', function () {
-    return [
-      {
-        type: 'output',
-        filter: function(source) {
-          var elements = $(source);
-          var output = [];
-          var headingLevel = null;
-          var tocId = null;
-          for (var i=0; i<elements.length; i++) {
-            var element = $(elements[i]);
-            var results = null;
+  function getHeaderEntries(sourceHtml) {
+    if (typeof window === 'undefined') {
+      return getHeaderEntriesInNodeJs(sourceHtml);
+    } else {
+      return getHeaderEntriesInBrowser(sourceHtml);
+    }
+  }
 
-            // Does the element consist only of [toc]?
-            // If so, we can replace this element with out list.
-            if (element.text().trim()=='[toc]') {
-              element = $('<ol>',{'class':'showdown-toc'});
-              headingLevel = null;
-              tocId = output.length;
-            }
+  function getHeaderEntriesInNodeJs(sourceHtml) {
+    var cheerio = require('cheerio');
+    var $ = cheerio.load(sourceHtml);
+    var headers = $('h1, h2, h3, h4, h5, h6');
 
-            // Does this item contain a [toc] with other stuff?
-            // If so, we'll split the element into two
-            else if (results = element.text().trim().match(/^([\s\S]*?)((?:\\)?\[toc\])([\s\S]*)$/)) {
+    var headerList = [];
+    for (var i = 0; i < headers.length; i++) {
+      var el = headers[i];
+      headerList.push(new TocEntry(el.name, $(el).text(), $(el).attr('id')));
+    }
 
-              // If there was a \ before the [toc] they're trying to escape it,
-              // so return the [toc] string without the \ and carry on. For
-              // some reason (I'm guessing a bug in showdown) you actually
-              // appear to need two \ (\\) in order to get this to show up for
-              // the filter. Leaving this code here anyway for now because it's
-              // "the right thing to do"(tm).
-              if (results[2][0]=='\\') {
-                element.text(results[1]+results[2].substr(1)+results[3]);
-              }
+    return headerList;
+  }
 
-              // Otherwise start building a new table of contents.
-              else {
-                var before = null;
-                var after = null;
+  function getHeaderEntriesInBrowser(sourceHtml) {
+    // Generate dummy element
+    var source = document.createElement('div');
+    source.innerHTML = sourceHtml;
 
-                // Create two of the same element.
-                if (element.prop('tagName')) {
-                  if (results[1].trim().length>0) {
-                    before = $('<'+element.prop('tagName')+'>').text(results[1]);
-                  }
-                  if (results[3].trim().length>0) {
-                    after = $('<'+element.prop('tagName')+'>').text(results[3]);
-                  }
-                }
+    // Find headers
+    var headers = source.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    var headerList = [];
+    for (var i = 0; i < headers.length; i++) {
+      var el = headers[i];
+      headerList.push(new TocEntry(el.tagName, el.textContent, el.id));
+    }
 
-                // Otherwise if there's no tagName assume it's a text node
-                // and create two of those.
-                else {
-                  if (results[1].trim().length>0) {
-                    before = document.createTextNode(results[1]);
-                  }
-                  if (results[3].trim().length>0) {
-                    after = document.createTextNode(results[3]);
-                  }
-                }
+    return headerList;
+  }
 
-                // Our new table of contents container.
-                var toc = $('<ol>',{'class':'showdown-toc'});
+  function TocEntry(tagName, text, anchor) {
+    this.tagName = tagName;
+    this.text = text;
+    this.anchor = anchor;
+    this.children = [];
+  }
 
-                // If there was text before our [toc], add that in
-                if (before) {
-                  output.push(before);
-                }
+  TocEntry.prototype.childrenToString = function() {
+    if (this.children.length === 0) {
+      return "";
+    }
+    var result = "<ul>\n";
+    for (var i = 0; i < this.children.length; i++) {
+      result += this.children[i].toString();
+    }
+    result += "</ul>\n";
+    return result;
+  };
 
-                // Keep track of where our current table is in the elements array.
-                tocId = output.length;
+  TocEntry.prototype.toString = function() {
+    var result = "<li>";
+    if (this.text) {
+      result += "<a href=\"#" + this.anchor + "\">" + this.text + "</a>";
+    }
+    result += this.childrenToString();
+    result += "</li>\n";
+    return result;
+  };
 
-                // If there was text after, push the contents onto the array and
-                // use the after part as our current element.
-                if (after) {
-                  output.push(toc);
-                  element = after;
-                }
+  function sortHeader(tocEntries, level) {
+    level = level || 1;
+    var tagName = "H" + level,
+      result = [],
+      currentTocEntry;
 
-                // Otherwise use the contents as the current element.
-                else {
-                  element = toc;
-                }
-
-                // Reset the heading level - we're going to start looking for new
-                // headings again
-                headingLevel = null;
-
-              }
-            }
-
-            // If we've started a table of contents, but have nothing in it yet,
-            // look for the first header tag we encounter (after the [toc]).
-            // That's going to be what we use as contents entries for this table
-            // of contents.
-            else if (tocId && !headingLevel && element.prop("tagName")) {
-              switch (element.prop("tagName")) {
-                case 'H1':
-                case 'H2':
-                case 'H3':
-                case 'H4':
-                case 'H5':
-                case 'H6':
-                  headingLevel = parseInt(element.prop('tagName').substr(1));
-                  break;
-              }
-            }
-
-            // If we know what header level we're looking for (either we just
-            // found it above, or we're continuing to look for more) then check to
-            // see if this heading should be added to the contents.
-            if (tocId && headingLevel) {
-              switch (element.prop('tagName')) {
-                case 'H1':
-                case 'H2':
-                case 'H3':
-                case 'H4':
-                case 'H5':
-                case 'H6':
-                  var thisLevel = parseInt(element.prop('tagName').substr(1));
-                  if (thisLevel==headingLevel) {
-                    output[tocId] = $(output[tocId]).append($('<li>').append($('<a>',{href:'#'+element.attr('id'),text:element.text()})));
-                  }
-                  // If we move up in what would be the document tree
-                  // (eg: if we're looking for H2 and we suddenly find an
-                  // H1) then we can probably safely assume that we want
-                  // the table of contents to end for this section.
-                  else if (thisLevel<headingLevel) {
-                    toc = null;
-                    tocId = null;
-                    headingLevel = null;
-                  }
-                  break;
-              }
-            }
-            // Push whatever element we've been looking at onto the output array.
-            output.push(element);
-          }
-          // Build some HTML to return
-          // Return it.
-          return $('<div>').append(output).html();
+    function push(tocEntry) {
+      if (tocEntry !== undefined) {
+        if (tocEntry.children.length > 0) {
+          tocEntry.children = sortHeader(tocEntry.children, level + 1);
         }
+        result.push(tocEntry);
       }
-    ];
-  });
+    }
+
+    for (var i = 0; i < tocEntries.length; i++) {
+      var tocEntry = tocEntries[i];
+      if (tocEntry.tagName.toUpperCase() !== tagName) {
+        if (currentTocEntry === undefined) {
+          currentTocEntry = new TocEntry();
+        }
+        currentTocEntry.children.push(tocEntry);
+      } else {
+        push(currentTocEntry);
+        currentTocEntry = tocEntry;
+      }
+    }
+
+    push(currentTocEntry);
+    return result;
+  }
+
+  return [{
+    type: 'output',
+    filter: function(sourceHtml) {
+      var headerList = getHeaderEntries(sourceHtml);
+
+      // No header found
+      if (headerList.length === 0) {
+        return sourceHtml;
+      }
+
+      // Sort header
+      headerList = sortHeader(headerList);
+
+      // Build result and replace all [toc]
+      var result = '<div class="toc">\n<ul>\n' + headerList.join("") + '</ul>\n</div>\n';
+      return sourceHtml.replace(/\[toc\]/gi, result);
+    }
+  }];
 }));
